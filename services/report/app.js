d2ac6887f46f98cc0c683e87bc973b5ef007a942
sergivaldo123@gmail.com <sergivaldo123@gmail.com>
2026-09-01 17:11:23 -0300

    implementação

--- services/report/app.js
+++ services/report/app.js
@@ -24,7 +24,13 @@ async function printReport() {
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
+    console.log(`SUCCESSFULLY SUBSCRIBED TO QUEUE: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {
+        const orderData = JSON.parse(msg.content)
+        updateReport(orderData.products)
+        printReport()
+    })
 } 
+
 
 consume()
